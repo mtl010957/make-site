5f9eec2fdef7cf8047a61b0ad031552f30f7eb15
Mike Loeffler <michael.t.loeffler@gmail.com>
2020-04-06 10:48:57 -0400

    Add no cache metadata to avoid problems with browsers

--- make_index.sh
+++ make_index.sh
@@ -10,6 +10,9 @@ cat <<EOH > index.html
 <html>
 <head>
 <title>${cam} ${type}</title>
+<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+<meta http-equiv="Pragma" content="no-cache" />
+<meta http-equiv="Expires" content="0" />
 </head>
 <body>
 <h1>${cam} ${type}</h1>
