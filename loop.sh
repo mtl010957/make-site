22e0c357ae9d94643bf5c8834fecedf7f5499d6a
Mike Loeffler <michael.t.loeffler@gmail.com>
2021-02-25 09:47:07 -0500

    Add looping script to run as standalone container

---
+++ loop.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Start rpcbind once to support nfs
+rpcbind
+
+while true
+do
+    echo "Make site at `date`"
+    mount -t nfs 192.168.13.5:/mnt/HD/HD_a2 /nas_a2 -o rw,timeo=600
+    /home/pi/make_site.sh
+    umount /nas_a2
+    
+    difference=$(($(date -d "00:20" +%s) - $(date +%s)))
+    
+    echo "Sleeping until next 00:20..."
+    if [ $difference -lt 0 ]
+    then
+        sleep $((86400 + difference))
+    else
+        sleep $difference
+    fi
+    
+done
